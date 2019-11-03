e944c527b81caa1fd54de3a091a326d3467ab02c
OrcaXS Suen <sun16281394@gmail.com>
2019-11-03 14:34:55 +0800

    Update Viewport

--- src/components/PIXI/Viewport.tsx
+++ src/components/PIXI/Viewport.tsx
@@ -3,18 +3,26 @@ import { Viewport } from 'pixi-viewport';
 
 const TYPE = 'Viewport';
 const behavior = {
-  create: props => new Viewport({
-    screenWidth: props.app.renderer.width,
-    screenHeight: props.app.renderer.height,
-    worldWidth: 1000,
-    worldHeight: 1000,
-    interaction: props.app.renderer.plugins.interaction,
+  create: (
+    { height, width, app }:
+    { height: number; width: number; app: PIXI.Application }
+  ): Viewport => new Viewport({
+    screenWidth: height,
+    screenHeight: width,
+    worldWidth: 3000,
+    worldHeight: 3000,
+    interaction: app.renderer.plugins.interaction,
   }).drag()
     .pinch()
     .wheel()
     .clampZoom({
       minWidth: 500, minHeight: 500, maxWidth: 8000, maxHeight: 8000,
-    })
+    }),
+  applyProps: (
+    instance: Viewport, _, { height, width }: { height: number; width: number }
+  ): void => {
+    instance.resize(width, height);
+  },
 };
 
 export default PixiComponent(TYPE, behavior);
